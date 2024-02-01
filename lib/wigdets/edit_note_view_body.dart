f23f7df779063fad663a78ea558d81dae3d506db
sherryahmos473 <sherry.ahmos@gmail.com>
2024-02-01 14:51:19 +0200

    finish ui

--- lib/wigdets/edit_note_view_body.dart
+++ lib/wigdets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/wigdets/custom_appbar.dart';
+import 'package:note_app/wigdets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -11,10 +12,16 @@ class EditNoteViewBody extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(
-            height: 40,
+            height: 50,
           ),
-          CustomAppBar(title: 'Edit',icon: Icons.check),
-          
+          CustomAppBar(title: 'Edit', icon: Icons.check),
+          SizedBox(height: 50),
+          CustomTextField(hintText: 'Title'),
+          SizedBox(height: 20),
+          CustomTextField(
+            hintText: 'Content',
+            maxLines: 5,
+          ),
         ],
       ),
     );
